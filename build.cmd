2457195d801223667900e72492bbdb0930ad35fe
Calendau GUQUET <kall.guquet@gmail.com>
2021-02-26 19:08:35 +0100

    Added StompClient in a new project, supporting apiKey and channel subscription

--- build.cmd
+++ build.cmd
@@ -4,7 +4,7 @@ echo ====== Cleaning build folder ======
 rm -f .build/*
 
 echo ====== Building syp.biz.SockJS.NET.Client ======
-dotnet build -c Release -o .build/ syp.biz\SockJS.NET\syp.biz.SockJS.NET.Client\
+dotnet build -c Release -o .build/ syp.biz\SockJS.NET\syp.biz.Stomp.Net.Client\
 
 echo ====== Pushing generated packages ======
 for %%v in (.build/*.nupkg) do nuget.exe push -Source "Synexie-packages" -ApiKey AzureDevOps .build/%%v
